a0d23ce66840670de79a1e844aac5bb7f39aca4d
Nicolas <nicolas.docolas1804@gmail.com>
2024-06-21 13:44:44 -0300

    teste de funcionamento do metodo de alocar nodos

--- main.c
+++ main.c
@@ -14,5 +14,9 @@ int main()
     }
 
     printf("Sucesso!\n");
+    void *aloc1 = mymemory_alloc(memory, 100);
+    void *aloc2 = mymemory_alloc(memory, 200);
+    void *aloc3 = mymemory_alloc(memory, 50);
+    printf("Alocou com sucesso!\n");
     return 0;
 }
