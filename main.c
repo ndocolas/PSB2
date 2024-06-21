aeffb3076ac5eb619d60614da2630780f7e7d5ab
Nicolas <nicolas.docolas1804@gmail.com>
2024-06-21 15:07:03 -0300

    teste do metodo mymemory_display

--- main.c
+++ main.c
@@ -12,14 +12,13 @@ int main()
         printf("Falhou!\n ");
         return 1;
     }
-
-    printf("Sucesso!\n");
+    mymemory_display(m);
     void *alloc1 = mymemory_alloc(m, 100);
     void *alloc2 = mymemory_alloc(m, 200);
     void *alloc3 = mymemory_alloc(m, 50);
-    printf("Alocou com sucesso!\n");
+    mymemory_display(m);
 
     mymemory_free(m, alloc2);
-    printf("Limpou com sucesso\n");
+    mymemory_display(m);
     return 0;
 }
