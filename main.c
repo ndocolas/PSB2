3891a186fd19887fd5fc248c6fd57d70f324c87f
Nicolas <nicolas.docolas1804@gmail.com>
2024-06-21 14:45:30 -0300

    teste da implementacao do metodo mymemory_free

--- main.c
+++ main.c
@@ -5,18 +5,21 @@ int main()
 {
     printf("Iniciando...\n");
     size_t tamanho = 500;
-    mymemory_t *memory = mymemory_init(tamanho);
+    mymemory_t *m = mymemory_init(tamanho);
 
-    if(memory == NULL)
+    if(m == NULL)
     {
         printf("Falhou!\n ");
         return 1;
     }
 
     printf("Sucesso!\n");
-    void *aloc1 = mymemory_alloc(memory, 100);
-    void *aloc2 = mymemory_alloc(memory, 200);
-    void *aloc3 = mymemory_alloc(memory, 50);
+    void *alloc1 = mymemory_alloc(m, 100);
+    void *alloc2 = mymemory_alloc(m, 200);
+    void *alloc3 = mymemory_alloc(m, 50);
     printf("Alocou com sucesso!\n");
+
+    mymemory_free(m, alloc2);
+    printf("Limpou com sucesso\n");
     return 0;
 }
